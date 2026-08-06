a62856eed40c61abbfa29e12973fe6be056bd86f
Marvin Okotie <155176572+Marvin-Okotie@users.noreply.github.com>
2026-08-06 11:51:34 -0400

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> seq_map;
+
+        for(auto& n: nums){
+            seq_map[n] ++;
+        }
+
+        int curr = 0;
+        int max = 0;
+        for(auto n : nums){
+            bool seq_begin = false;
+            if(!(seq_map.count(n-1))){
+                seq_begin = true;
+            }
+            int k = 0;
+            if(seq_begin){
+                //curr++;
+                seq_begin = false;
+                k = n;
+                while(seq_map.count(k)){
+                    curr++;
+                    k = k+1;
+                }
+                if(curr > max) max = curr;
+            }
+            curr = 0;
+            n = k - 1;
+        }
+
+        return max;
+    }
+};
