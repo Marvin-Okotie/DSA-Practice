924f0ae01e601a0dc1557bf5cb934d3f3b4461f5
Marvin Okotie <155176572+Marvin-Okotie@users.noreply.github.com>
2026-08-11 09:30:56 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0;
+        int res = 0;
+        int min = prices[0];
+        for(int r = 0; r < prices.size(); ++r){
+            int profit = 0;
+            if(prices[r] < min){
+                min = prices[r];
+                l = r;
+            }
+            if(prices[l] < prices[r]){
+                profit = prices[r] - prices[l];
+            }
+
+            res = max(res, profit);
+        }
+
+        return res;
+    }
+};
